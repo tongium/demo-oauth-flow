bb3d9a4f33223dcf59ac0663c4baac8bd0f5e4d4
Tong Warinthorn <tong.w@finnomena.com>
2023-02-15 17:29:37 +0700

    build: removed github action

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  base: "/glowing-meme/",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
