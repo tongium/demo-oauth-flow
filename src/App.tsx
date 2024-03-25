80e4f1fbbe9d6214aff2020e3de68bcd3db303c0
Warinthorn Tuadsin <warinthorn.tua@gmail.com>
2024-03-25 19:28:53 +0700

    feat: updated github url

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App: Component = () => {
   return (
     <div class={styles.App}>
       <nav class='w-1/1 text-right p-1 text-sm'>
-        <a href="https://github.com/tongium/glowing-meme" target="_blank">Github</a>
+        <a href="https://github.com/tongium/demo-oauth-flow" target="_blank">Github</a>
       </nav>
       <main class='flex flex-col items-center justify-center pt-8'>
         <Show
